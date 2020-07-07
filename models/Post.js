3ba9f57b6b939bea7069074718f991104bb35c8d
DESTERIO-RASMENTO <desochudi@gmail.com>
2020-07-07 16:29:19 +0300

    -m

--- models/Post.js
+++ models/Post.js
@@ -107,6 +107,7 @@ Post.reusablePostQuery = function (uniqueOperations,visitorId) {
         //clean up author property in each post object
         posts = posts.map(function (post) {
             post.isVisitorOwner = post.authorId.equals(visitorId)
+            post.authorId = undefined
             post.author = {
                 username: post.author.username,
                 avatar: new User(post.author, true).avatar
@@ -156,5 +157,20 @@ Post.delete = function(postIdToDelete,currentUserId){
             reject()
         }
     })
+}
+
+Post.search = function(searchTerm){
+return new Promise(async(resolve,reject)=>{
+    if (typeof(searchTerm) == "string") {
+       let posts =await Post.reusablePostQuery([
+           {$match: {$text: {$search: searchTerm}}},
+           {$sort: {score: {$meta:"textScore"}}}
+       ]) 
+       resolve(posts)
+    } else {
+        reject()
+    }
+
+})
 }
 module.exports = Post
